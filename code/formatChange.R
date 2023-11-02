886646be30ae42410c011340df18fc45c60db2f4
PChunark <puttipongchunark@gmail.com>
2023-11-02 16:32:05 +0700

    Finish programing

--- code/formatChange.R
+++ code/formatChange.R
@@ -8,40 +8,10 @@ library(plotly)
 library(writexl) # for reading excel
 library(openxlsx)
 
-profile1 <- read_excel("rawdata/GenerationProfile_PF.xlsx",
-           sheet = "Generation profile",
-           range = "B7:LYB100",
-           col_names = c("vspp", "in_cap_mw","mwh",1:8760)) %>% 
-  select(-"in_cap_mw", -"mwh") %>% 
-  pivot_longer(-"vspp", names_to = "hourly", values_to = "mw") %>% 
-  group_by(vspp) %>% 
-  mutate(datetime = seq(as.POSIXct("2019-01-01 00:00:00", format="%Y-%m-%d %H:%M:%S"),
-                        as.POSIXct("2019-12-31 23:30:00", format="%Y-%m-%d %H:%M:%S"),
-                        by="1 hour"),
-         year = year(datetime),
-         month = month(datetime),
-         day = day(datetime),
-         hour = hour(datetime)) %>%
-  mutate(seqe = rep(1:24,365)) %>%  #daily profile
-  filter(vspp == "VSPP Solar C")
 
-a1<-ggplotly(profile1 %>% ggplot() + geom_line(aes(x=datetime, y = mw)))
+#Program function
 
-tt<- profile1 %>% 
-  group_by(seqe) %>% 
-  mutate(newmw = mw/2) %>% ungroup() %>% 
-  select(newmw)
-
-# USE this
-tt%>% slice(rep(1:n(), each = 2))
-  # data.frame(rep(tt,2))
-
-
-path <- "rawdata/GenerationProfile_PF.xlsx"
-startyear <- "2019-01-01 00:00:00"
-endyear <- "2019-12-31 23:30:00"
-
-adjProfFrm1HrTo30Min <-
+adjGenProFr1HrTo30Min <-
   function(path,startyear,endyear){
     profile <- read_excel(path, #Input from user
                           sheet = "Generation profile",
@@ -58,7 +28,8 @@ adjProfFrm1HrTo30Min <-
                                format="%Y-%m-%d %H:%M:%S", tz = "UTC"),
                  by="30 min")
     
-    # temp1<-
+    
+    temp1<-
       profile %>% 
       # group_by(seqe) %>% 
       # mutate(newmw = mw/2) %>% 
@@ -74,15 +45,19 @@ adjProfFrm1HrTo30Min <-
              month = month(datetime),
              date = day(datetime)) %>% 
       # mutate(seq1 = rep(1:24, 0.5, 365))
-      filter(profileType == "VSPP Solar C",
-             month == 1,
-             date == 1) %>%
+      # filter(profileType == "VSPP Solar C") %>%
       # filter(profileType %in% c("VSPP Solar C", "VSPP Solar NE")) %>% 
-      mutate(check = seq2 %% 2 == 0) %>% #Check if it is odd number
-      replace()
-        newmw2 = if_else(check == T, 
-                              slider::slide_dbl(mw, ~mean(.x), .before = 1, .after = 1),
-                              mw))
+      mutate(check = seq2 %% 2 == 0, #Check if it is odd number
+             newmw2 = replace(mw, check == T, NA), #replace mw when check == true with NA
+             newmw2 = if_else(check == T, # Calcuate mean of newmw2 when check is TRUE
+                              slider::slide_dbl(newmw2, 
+                                                ~mean(.x, na.rm = T), #Calculate mean by excluding the NA value 
+                                                .before = 1, 
+                                                .after = 1),
+                              newmw2)
+                              ) 
+    temp2 <- data.frame(temp1)
+      
     data <-
       temp1 %>% 
       select(profileType, 
@@ -94,8 +69,8 @@ adjProfFrm1HrTo30Min <-
              weekType, 
              mw = newmw2)
     
-    energy <- temp1 %>% group_by(profileType) %>% mutate(energy = (sum(newmw2)/2))
-              
+    energy <- data %>% group_by(profileType) %>% mutate(energy = (sum(mw)/2))
+          
     
     # a2 <- ggplotly(
     #   tt %>% 
@@ -118,28 +93,9 @@ adjProfFrm1HrTo30Min <-
                colWidths = "auto",
                headerStyle = hs)
   
-}
-yt <- test(path, startyear,endyear)
+  return(list(data=data,energy=energy))
+  }
 
-toy_data <- tibble(
-  var_missing = rnorm(mean = 10, sd = 3, n = 360),
-  region = rep(c("A", "B", "C"), each = 2, times = 60),
-  gender = rep(c("Male","Female"), times = 180),
-  year = rep(2010:2014, each = 72),
-  month = rep(1:12, each = 6, times = 5)
-) %>%
-  mutate(month = month(month, label = TRUE))
 
-toy_data %>% 
-  group_by(region, gender, month) %>% 
-  mutate(
-    var_missing = 
-      if_else(
-        is.na(var_missing),
-        slider::slide_dbl(var_missing, mean, .before = 2, .after = -1),
-        var_missing
-      )
-  ) %>% 
-  ungroup()
+data <- adjGenProFr1HrTo30Min(path, startyear,endyear)
 
-slider::slide_dbl(mw, ~mean(.x), .before = 1, .after = 1)
