6985e53fae3d65cf2e28332babf2d7a3ea4dd326
PChunark <puttipongchunark@gmail.com>
2023-11-07 10:27:31 +0700

    Categorize the code reading

--- code/executeCode.R
+++ code/executeCode.R
@@ -1,3 +1,11 @@
+#User inout reading ####
 source("code/userInput.R")
+
+# Function ####
+## Getting executime function ####
+source("code/executeTimeFunc.R")
+## Profile format changing ####
 source("code/formatChange.R")
+
+# Visualization setting ####
 source("code/themeSetting.R")
