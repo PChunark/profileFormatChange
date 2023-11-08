f9ee4d71c7f2bc8cd6d11a1c2dfbcce173639720
PChunark <puttipongchunark@gmail.com>
2023-11-08 16:15:34 +0700

    Delete test batch files

--- batchtest2.bat
+++
@@ -1,3 +0,0 @@
-R CMD BATCH "D:\Rstudio\profileFormatChange\code\userInput.R" "D:\Rstudio\profileFormatChange\code\executeTimeFunc.R"
-
-pause
--- batchtest3.bat
+++
@@ -1,6 +0,0 @@
-cd "D:\Rstudio\profileFormatChange\code"
-
-RScript userInput.R
-RScript executeTimeFunc.R
-
-pause
